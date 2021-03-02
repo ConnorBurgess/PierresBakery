7cc179c5abbd7d20332eab9f1cfbd7e9340e00b0
Connor Burgess <connorburgesscodes@gmail.com>
2021-03-02 01:28:49 -0800

    update logic in Bakery.cs

--- bakery/models/Bakery.cs
+++ bakery/models/Bakery.cs
@@ -1,16 +1,19 @@
 using System;
-using System.Collections.Generic;
 namespace Bakery.Models
 {
   public class Bread
   {
     public static int breadCounter { get; set; }
     public static int breadCost { get; set; }
+    public static int freeBread { get; set; }
 
-    public static int BreadCost(int num)
+    public static int TotalBreadCost(int num)
     {
-      Bread.breadCost = (breadCounter / 2) >= 1 ? (int)(breadCounter / 2) : 0;
-      return Bread.breadCost;
+      Bread.breadCounter = num;
+      Bread.freeBread = (breadCounter / 2) >= 1 && (breadCounter % 2 != 0) ? (int)(breadCounter / 2) 
+        : (breadCounter / 2) >= 1 && (breadCounter % 2 == 0) ? (breadCounter / 2) - 1
+          : 0;
+      return (breadCounter - freeBread) * 5;
     }
   }
   public class Pastry
@@ -19,9 +22,11 @@ namespace Bakery.Models
     public static double pastryCounter { get; set; }
     public static double PastryCost(double num)
     {
+      Pastry.pastryCounter = num;
       Pastry.pastryCost = (pastryCounter % 3) == 0 ? (pastryCounter / 3) * 5
-        : pastryCounter > 3 ? Math.Floor(pastryCounter / 3)* 5 + (pastryCounter % 3) * 2
+        : pastryCounter > 3 ? Math.Floor(pastryCounter / 3) * 5 + (pastryCounter % 3) * 2
           : pastryCounter * 2;
+
       return Pastry.pastryCost;
     }
   }
