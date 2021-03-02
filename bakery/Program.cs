82039f8cfcece3108a2fcaf9752622492f80cbb5
Connor Burgess <connorburgesscodes@gmail.com>
2021-03-02 00:16:58 -0800

    add more functionality to terminal UI

--- bakery/Program.cs
+++ bakery/Program.cs
@@ -7,12 +7,27 @@ namespace Main
   public class Program
   {
     public static void Main(){
-      Console.Write("Welcome to Pierre's Bakery!");
+      Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
       Menu();
     }
 
     public static void Menu(){
       Console.WriteLine("Would you like to make a new bread or pastry purchase? (type: bread, pastry, or exit");
+      string menuInput = Console.ReadLine();
+      switch(menuInput)
+      { 
+        case "bread":
+        Bread();
+        break;
+
+    }
+
+  }
+    public static void Bread() {
+      Console.WriteLine("How many bread would you like to purchase? Single loaf: $5; Current sale: Buy 2, get 1 free!");
+      string breadNum = Console.ReadLine();
     }
   }
 }
