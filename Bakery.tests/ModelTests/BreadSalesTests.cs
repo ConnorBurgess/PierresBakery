8b98b91f1b4520553687fc2d8b0c98efb29086dc
Connor Burgess <connorburgesscodes@gmail.com>
2021-03-02 21:54:18 -0800

    separate classes and tests

---
+++ Bakery.tests/ModelTests/BreadSalesTests.cs
@@ -0,0 +1,28 @@
+using BreadSales.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace BreadSales.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void TotalBreadCost_BuyTwoGetOneFree_True()
+    {
+      Bread newBread = new Bread();
+      Bread.TotalBreadCost(3);
+      Assert.AreEqual(1, Bread.freeBread);
+    }
+
+  }
+}
--- Bakery.tests/ModelTests/PastrySalesTests.cs
+++ Bakery.tests/ModelTests/PastrySalesTests.cs
@@ -1,36 +1,16 @@
-using Bakery.Models;
+using PastrySales.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
-namespace Bakery.Tests
+namespace PastrySales.Tests
 {
-  [TestClass]
-  public class BreadTests
-  {
-
-    [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfBread_Bread()
-    {
-      Bread newBread = new Bread();
-      Assert.AreEqual(typeof(Bread), newBread.GetType());
-    }
-
-    [TestMethod]
-    public void TotalBreadCost_BuyTwoGetOneFree_True()
-    {
-      Bread newBread = new Bread();
-      Bread.TotalBreadCost(3);
-      Assert.AreEqual(1, Bread.freeBread);
-    }
-
-  }
   [TestClass]
   public class PastryTests
   {
 
     [TestMethod]
-    public void PastryConstructor_CreatesInstanceOfBread_Bread()
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
       Pastry newPastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
