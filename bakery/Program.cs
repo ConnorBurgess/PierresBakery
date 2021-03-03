d572a6e8f5da7f6768851a1fcc85ae375b7acde0
Connor Burgess <connorburgesscodes@gmail.com>
2021-03-02 21:42:20 -0800

    add more stylistic changes to terminal UI

--- bakery/Program.cs
+++ bakery/Program.cs
@@ -6,13 +6,20 @@ namespace Main
 {
   public class Program
   {
+
     public static void Main()
     {
+      Random randnum = new Random();
+      int num = randnum.Next(7);
+      string[] frenchPhrases = { "Dis-moi ce que tu manges, je te dirai qui tu es.", "Un bon repa doit commencer par la faim.", "Chaque cuisinier a sa sauce!", "Quand le vin est tiré, il faut le boire.", "Un bon repas adoucit l'esprit et régénère le corps.", "Aprés bon vin, bon coussin..." };
+      string[] frenchPhrasesTranslated = { "Tell me what you eat, and I'll tell you who you are.", "A good meal starts with hunger", "Each cook has his own sauce!", "When the wine is poured, it must be drunk.", "A good meal softens the mind and regenerates the body.", "After a good wine, a good pillow..." };
       Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       Console.ForegroundColor = ConsoleColor.Blue;
       Console.WriteLine("         Welcome to Pierre's Bakery!");
       Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+      Console.WriteLine($"Pierre: {frenchPhrases[num]}");
+      Console.WriteLine($"({frenchPhrasesTranslated[num]})");
       Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       Console.WriteLine("Bread: $5; Current sale: Buy 2, get 1 free!");
       Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
